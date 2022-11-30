709ce7dc6132f883e78f1ec2dc5063f7ccad0e2e
Braden James <97642471+bradenbjames@users.noreply.github.com>
2022-11-30 04:13:13 -0600

    changes

--- BookStore_James_Brewster/AdminEditOrder.xaml.cs
+++ BookStore_James_Brewster/AdminEditOrder.xaml.cs
@@ -25,4 +25,111 @@ namespace BookStore_James_Brewster
 			InitializeComponent();
 		}
 	}
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		AdminAddBook a = new AdminAddBook();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		AdminBookBrowser a = new AdminBookBrowser();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_2(object sender, RoutedEventArgs e)
+	{
+		AdminCreateCategory a = new AdminCreateCategory();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_3(object sender, RoutedEventArgs e)
+	{
+		//Admineditauthordata
+	}
+
+	private void Button_Click_4(object sender, RoutedEventArgs e)
+	{
+		//admineditbook
+	}
+
+	private void Button_Click_5(object sender, RoutedEventArgs e)
+	{
+		AdminViewAuthors a = new AdminViewAuthors();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_6(object sender, RoutedEventArgs e)
+	{
+		AdminViewCustomers a = new AdminViewCustomers();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_7(object sender, RoutedEventArgs e)
+	{
+		AdminViewOrders a = new AdminViewOrders();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_8(object sender, RoutedEventArgs e)
+	{
+		AdminViewSuppliers a = new AdminViewSuppliers();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_9(object sender, RoutedEventArgs e)
+	{
+		CreateAccount a = new CreateAccount();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_10(object sender, RoutedEventArgs e)
+	{
+		CustomerBookBrowser a = new CustomerBookBrowser();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_11(object sender, RoutedEventArgs e)
+	{
+		CustomerUpdateAccount a = new CustomerUpdateAccount();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_12(object sender, RoutedEventArgs e)
+	{
+		CustomerViewOrders a = new CustomerViewOrders();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_13(object sender, RoutedEventArgs e)
+	{
+		CreateAccount a = new CreateAccount();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_14(object sender, RoutedEventArgs e)
+	{
+		Login a = new Login();
+		a.Show();
+		this.Close();
+	}
+
+	private void Button_Click_15(object sender, RoutedEventArgs e)
+	{
+		MainWindow a = new MainWindow();
+		a.Show();
+		this.Close();
+	}
 }
