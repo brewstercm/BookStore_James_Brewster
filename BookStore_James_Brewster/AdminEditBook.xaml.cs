15df80d2bad697869f1e00e570d42a069407b9ca
Braden James <97642471+bradenbjames@users.noreply.github.com>
2022-11-29 22:58:18 -0600

    add pages

---
+++ BookStore_James_Brewster/AdminEditBook.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookStore_James_Brewster
+{
+	/// <summary>
+	/// Interaction logic for AdminEditBook.xaml
+	/// </summary>
+	public partial class AdminEditBook : Page
+	{
+		public AdminEditBook()
+		{
+			InitializeComponent();
+		}
+	}
+}
